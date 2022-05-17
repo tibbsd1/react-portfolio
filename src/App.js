236ed5806b3cd89fabd4b312183964126cd269b3
tibbsd1 <tibbsd1@gmail.com>
2022-05-17 19:53:24 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         Darrell's full stack portfolio coming soon to React! &#129305;&#127999;
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Alex is awesome.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
