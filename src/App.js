7af9f49d826fb1a2b2b0f21914b621142fa0d486
tibbsd1 <tibbsd1@gmail.com>
2022-07-29 15:57:23 -0400

    footer w/contact

--- src/App.js
+++ src/App.js
@@ -9,14 +9,18 @@ function App() {
     <div className="App">
       <section className="hero is-fullheight" style={{ backgroundColor: "#101010" }}>
         <div className="hero-body container">
-          <div className="box title-box">
-            <p className="title is-spaced dt-title">
-              Darrell Tibbs
-            </p>
-            <p className="subtitle dt-subtitle">
-              JavaScript Developer
-            </p>
-          </div>
+          <a href='#projects'>
+
+            <div className="box title-box">
+              <p className="title is-spaced dt-title">
+                Darrell Tibbs
+              </p>
+              <p className="subtitle dt-subtitle">
+                JavaScript Developer
+              </p>
+            </div>
+          </a>
+
         </div>
       </section>
       <section>
@@ -30,9 +34,26 @@ function App() {
 
       </section>
 
-      {data && data.map(p => (
-        <Project title={p.title} imageUrl={`${process.env.PUBLIC_URL}/assets/${p.image}`} skills={p.tools} link={p.link} />
-      ))}
+      <section id='projects'>
+
+        {data.map(p => (
+          <Project title={p.title} imageUrl={`${process.env.PUBLIC_URL}/assets/${p.image}`} skills={p.tools} link={p.link} />
+        ))}
+      </section>
+      <footer className='footer'>
+        <div className='level'>
+          <figure className="level-left image is-48x48">
+            <img className="level-item is-rounded" src={`${process.env.PUBLIC_URL}assets/headshot.jpg`} />
+            <div className='level-right'>
+              <ul>
+                <li>Email: tibbsd1@gmail.com</li>
+                <li>Linkedin:<a href='http://linkedin.com/in/darrelltibbs' target='_blank' rel='noreferrer'> linkedin.com/in/darrelltibbs</a></li>
+                <li>Github:<a href='http://github.com/tibbsd1' target='_blank' rel='noreferrer'> github.com/tibbsd1</a></li>
+              </ul>
+            </div>
+          </figure>
+        </div>
+      </footer>
     </div>
   );
 }
